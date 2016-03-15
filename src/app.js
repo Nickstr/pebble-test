02dff9df4bd277613ab600f8aab545c31a0aae7c
Nick Spelt <nickstr@nickstr.nl>
2016-03-15 11:38:02 +0100

    force update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var UI = require('ui');
 var main = new UI.Card({
     title: 'Pebble.js',
     icon: 'images/menu_icon.png',
-    subtitle: 'Hello Nick!',
+    subtitle: 'Hello Mitchell!',
     body: 'Is it updated?!',
     subtitleColor: 'indigo', // Named colors
     bodyColor: '#9a0036' // Hex colors
